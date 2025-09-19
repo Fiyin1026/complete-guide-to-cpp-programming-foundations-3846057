fb4c876b8eb11032c688cc8c7e369ec60cdb5456
Fiyin1026 <stewardj14@gmail.com>
2025-09-19 15:35:35 +0000

    added a comment, and I'm learning software

--- src/Ch00/CodeDemo.cpp
+++ src/Ch00/CodeDemo.cpp
@@ -1,5 +1,5 @@
 // Complete Guide to C++ Programming Foundations
-// Exercise 00_03 with my modifications
+// Exercise 00_03 with my changes and learning  
 // Using the Exercise Files in GitHub Codespaces, by Eduardo Corpeño 
 
 #include <iostream>
